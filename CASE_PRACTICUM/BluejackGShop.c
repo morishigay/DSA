b76b07429b7c837dd9f4187d4a55a143f2d62514
morishigay <40287157+morishigay@users.noreply.github.com>
2026-06-24 17:46:37 +0700

    Update BluejackGShop.c
    something was wrong

--- CASE_PRACTICUM/BluejackGShop.c
+++ CASE_PRACTICUM/BluejackGShop.c
@@ -1,44 +1,44 @@
 //ABOUT AVL TREE
-#define _GNU_SOURCE //this is for strdup. you can also use #define _POSIX_C_SOURCE_200809L
+#define _POSIX_C_SOURCE 200809L //for strdup, or you can use #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_TITLE 100000
+#define MAX_NAME 100000
 #define MAX_GENRE 100
 
-//PART 1: AVL TREE STRUCTURE AND NODE CREATION
-
+//AVL tree node
 typedef struct Node{
     char* title; //KEY
     char* genre;
     int stock;
+
+    //what an AVL tree must have idk what to call them
     struct Node* left;
     struct Node* right;
     int height;
 } Node;
 
-Node* createNode(Node* node, const char* title, const char* genre, int stock){
+//create AVL tree node
+Node* createNode(char* title, char* genre, int stock){
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->title = strdup(title);
     newNode->genre = strdup(genre);
-    newNode->stock = stock;
+    newNode->stock = 0;
     newNode->left = newNode->right = NULL;
     newNode->height = 1;
     return newNode;
 }
 
-//PART 2: VALIDATING INPUTS
-
-//must be between 5 and 25 char, unique (for utility)
+//must be between 5 and 25, unique
 bool isUnique(Node* root, const char* title){
-    if (!root) return true;
+    if (root == NULL) return true;
     if (strcmp(root->title, title) == 0) return false;
     return isUnique(root->left, title) && isUnique(root->right, title);
 }
 
-bool validTitle(Node* root, const char* title){
+bool validName(Node* root, const char* title){
     int len = strlen(title);
     if (len < 5 || len > 25){
         return false;
@@ -46,15 +46,15 @@ bool validTitle(Node* root, const char* title){
     return isUnique(root, title);
 }
 
-void getTitle(Node* root, char* title){
+void getGameName(Node* root, char* title){
     do{
         printf("Input game title[5-25][unique]: ");
-        if (fgets(title, MAX_TITLE, stdin) == NULL) return;
+        if (fgets(title, MAX_NAME, stdin) == NULL) return;
         title[strcspn(title, "\n")] = '\0';
-    } while (!validTitle(root, title));
+    } while (!validName(root, title));
 }
 
-//must be Action, RPG, Adventure, Card Game (for utility)
+//must be the following: Action, RPG, Adventure, Card Game
 bool validGenre(const char* genre){
     if (strcmp(genre, "Action") != 0 && strcmp(genre, "RPG") != 0 && strcmp(genre, "Adventure") != 0 && strcmp(genre, "Card Game") != 0){
         return false;
@@ -62,15 +62,15 @@ bool validGenre(const char* genre){
     return true;
 }
 
-void getGenre(char* genre){
+void getGameGenre(char* genre){
     do{
-        printf("Input game genre[]: ");
+        printf("Input game genre[Action|RPG|Adventure|Card Game]: ");
         if (fgets(genre, MAX_GENRE, stdin) == NULL) return;
         genre[strcspn(genre, "\n")] = '\0';
     } while (!validGenre(genre));
 }
 
-//must be greater or equal than 1 (for utility)
+//must be greater or equal than 1
 bool validStock(int stock){
     if (stock < 1){
         return false;
@@ -78,55 +78,40 @@ bool validStock(int stock){
     return true;
 }
 
-void getStock(int* stock){
+void getGameStock(int* stock){
     char input[20];
     do{
-        printf("Input stock[>= 1]: ");
-        if (fgets(input, 20, stdin) == 0) return;
-        input[strcspn(input, "\n")] = '\n';
+        printf("Input game stock[>= 1]: ");
+        if (fgets(input, 20, stdin) == NULL) return 0;
+        input[strcspn(input, "\n")] = '\0';
         *stock = atoi(input);
-    } while(!validStock(stock));
+    } while (!validStock(*stock));
 }
 
-//PART 3: AVL TREE MECHANICS FOR INSERTION, DELETION AND TRAVERSAL/PRINTING
-
-/* In C, max and min are not built-in functions. We'll have to implement it from scratch but it's pretty easy rlly.
-int max(int a, int b){
-    return (a > b) ? a : b;
-}
-
-int min(int a, int b){
-    return (a < b) ? a : b;
-}*/
-
-//utility to get height
+//AVL tree mechanics for insertion
 int getHeight(Node* root){
     if (root == NULL) return 0;
     return root->height;
 }
 
-//utility for checking balance of a node
-int getBalance(Node* root){
-    if (root == NULL) return 0;
-    return getHeight(root->left) - getHeight(root->right);
+int getMax(int a, int b){
+    return (a > b) ? a : b;
 }
 
-//utility for right rotation in y subtree
 Node* rightRotate(Node* y){
     Node* x = y->left;
     Node* t2 = x->right;
 
     //perform rotation
     x->right = y;
-    y->left = t2; 
+    y->left = t2;
 
     //update heights
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = getMax(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = getMax(getHeight(x->left), getHeight(x->right)) + 1;
     return x;
 }
 
-//utility for left rotation in x subtree
 Node* leftRotate(Node* x){
     Node* y = x->right;
     Node* t2 = y->left;
@@ -136,19 +121,27 @@ Node* leftRotate(Node* x){
     x->right = t2;
 
     //update heights
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = getMax(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = getMax(getHeight(y->left), getHeight(y->right)) + 1;
     return y;
 }
 
-//insert a node in an AVL tree
-Node* insert(Node* root, char* title){
-    //basic BST insertion
-    int res = strcmp(title, root->title);
-    if (res < 0){
-        root = insert(root->left, title);
-    } else if (res > 0){
-        root = insert(root->right, title);
+int getBalance(Node* root){
+    if (root == NULL) return 0;
+    return getHeight(root->left) - getHeight(root->right);
+}
+
+//insert a game (THE TITLE IS THE KEY)
+Node* insert(Node* root, char* title, char* genre, int stock){
+    //usual BST insertion
+    if (root == NULL){
+        return createNode(title, genre, stock);
+    }
+    int res = strcmp(root->title, title);
+    if (res > 0){
+        root->left = insert(root->left, title, genre, stock);
+    } else if (res < 0){
+        root->right = insert(root->right, title, genre, stock);
     } else{
         return root;
     }
@@ -156,34 +149,35 @@ Node* insert(Node* root, char* title){
     //update height
     root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
 
-    //check if the following node is balanced
+    //call getBalance to check if the node became unbalanced
     int balance = getBalance(root);
 
-    //if not, there will be 4 cases
+    //if unbalanced, there are 4 cases
     //1. LL case
-    if (balance > 1 && res < 0){
+    if (balance > 1 && strcmp(title, root->left->title) < 0){
         return rightRotate(root);
     }
 
     //2. RR case
-    if (balance < -1 && res > 0){
+    if (balance < -1 && strcmp(title, root->right->title) > 0){
         return leftRotate(root);
     }
+
     //3. LR case
-    if (balance < 1 && res < 0){
+    if (balance > 1 && strcmp(title, root->left->title) > 0){
         root->left = leftRotate(root);
         return rightRotate(root);
     }
-
+    
     //4. RL case
-    if (balance > -1 && res > 0){
+    if (balance < -1 && strcmp(title, root->right->title) < 0){
         root->right = rightRotate(root);
         return leftRotate(root);
     }
     return root;
 }
 
-//search a node
+//search a game (TITLE IS THE KEY)
 Node* search(Node* root, char* title){
     if (root == NULL) return NULL;
     int res = strcmp(root->title, title);
@@ -195,99 +189,104 @@ Node* search(Node* root, char* title){
     return search(root->right, title);
 }
 
-//delete a node in an AVL tree
-Node* remove(Node* root, char* title){
-    //basic BST deletion
-    if (root == NULL){
-        return root;
+Node* getMin(Node* root){
+    if (root->left != NULL){
+        return getMin(root->left);
     }
+    return root;
+}
 
+//delete a game
+Node* removeGame(Node* root, char* title){
+    //usual BST deletion
+    if (root == NULL) return root;
     int res = strcmp(root->title, title);
-    if (res < 0){
-        root->left = remove(root->left, title);
-    } else if (res > 0){
-        root->right = remove(root->right, title);
+    if (res > 0){
+        root = removeGame(root->left, title);
+    } else if (res < 0){
+        root = removeGame(root->right, title);
     } else{
-        if (!root->left || !root->right){
-            Node* temp = root->left ? root->left : root->right;
-            if (!temp){
-                temp = root;
-                root = NULL;
-            } else{
-                *temp = *root;
-                free(temp);
-            }
+        if (root->left == NULL && root->right == NULL){
+            free(root);
+            return NULL;
+        } else if (root->left != NULL && root->right != NULL){
+            Node* temp = getMin(root->right);
+            free(root->title);
+            free(root->genre);
+            free(temp);
+            root->title = strdup(temp->title);
+            root->genre = strdup(temp->genre);
+            root->stock = temp->stock;
+            root->right = remove(root->right, temp->title);
         } else{
-            Node* temp = min(root->right);
-            strncpy(temp->title, root->title, sizeof(temp->title) - 1);
-            root->right = remove(root->left, temp->title);
+            Node* temp = (root->left != NULL) ? root->left : root->right;
+            free(root->title);
+            free(root->genre);
+            free(root);
+            return temp;
         }
     }
 
     //update height
     root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
 
-    //check if the following node is balanced
+    //call getBalance to check if the node became unbalanced
     int balance = getBalance(root);
 
-    //if not, there will be 4 cases
+    //if unbalanced, there are 4 cases
     //1. LL case
-    if (balance > 1 && res < 0){
+    if (balance > 1 && strcmp(title, root->left->title) < 0){
         return rightRotate(root);
     }
 
     //2. RR case
-    if (balance < -1 && res > 0){
+    if (balance < -1 && strcmp(title, root->right->title) > 0){
         return leftRotate(root);
     }
+
     //3. LR case
-    if (balance < 1 && res < 0){
+    if (balance > 1 && strcmp(title, root->left->title) > 0){
         root->left = leftRotate(root);
         return rightRotate(root);
     }
-
+    
     //4. RL case
-    if (balance > -1 && res > 0){
+    if (balance < -1 && strcmp(title, root->right->title) < 0){
         root->right = rightRotate(root);
         return leftRotate(root);
     }
     return root;
 }
 
-//print all games (in-order)
-void viewAllGames(Node* root){
+//view all games in inorder method
+void viewGames(Node* root){
     if (root == NULL){
         printf("Warehouse is empty !\n");
-        printf("Press Enter to continue ...\n"); getchar();
+        printf("Press Enter to continue...\n"); getchar();
     } else{
-        printf("---------------------------------------------------------\n");
-        printf("Game Title               | Game Genre      | Game Stock |\n");
-        viewAllGames(root->left);
-        printf("| %-15s | %-6s | %-4d |\n");
-        viewAllGames(root->right);
-        printf("---------------------------------------------------------\n");
+        printf("----------------------------------------------------------------\n");
+        printf("| Game Title                  | Game Genre      | Game Stock   |\n");
+        printf("----------------------------------------------------------------\n");
+        viewGames(root->left);
+        printf("|%-25s | %-15s | %10d |\n");
+        viewGames(root->right);
+        printf("----------------------------------------------------------------\n");
     }
 }
 
-//delete a game
-Node* remove(Node* root, char* title){
-    
-}
-
-//freeeeeeeeee
+//free all games
 void freeGames(Node* root){
     if (root == NULL) return;
-    freeTree(root->left);
-    freeTree(root->right);
+    freeGames(root->left);
+    freeGames(root->right);
     free(root);
 }
 
-//PART 4: MENU FOR ALL FUNCTIONS IMPLEMENTED ABOVE
-
 void menu(){
     Node* root = NULL;
-    char* title, *genre;
     int choice, stock;
+    char title[MAX_NAME];
+    char genre[MAX_GENRE];
     do{
         printf("Bluejack GShop\n");
         printf("==============\n");
@@ -295,52 +294,59 @@ void menu(){
         printf("2. View Game\n");
         printf("3. Update Stock\n");
         printf("4. Exit\n");
-        printf(">> "); scanf("%d", &choice);
+        scanf("%d", &choice);
 
-        switch(choice){
-            case 1:{ //INSERT GAME
-                getTitle(root, title);
-                getGenre(genre);
-                getStock(stock);
-                root = insert(root, title);
-                printf("Insert success\n");
-            }
+        switch (choice){
+        case 1:{ //insert game
+            getGameName(root, title);
+            getGameGenre(genre);
+            getGameStock(stock);
+            root = insert(root, title, genre, stock);
+            printf("Insert success !\n");
+        }
+        break;
+        case 2: //view game
+            viewGames(root);
             break;
-            case 2: //VIEW GAME
-                viewAllGames(root);
-                break;
-            case 3:{ //UPDATE STOCK
-                getTitle(root, title);
-                Node* searched = search(root, title);
-                if (searched == NULL){ //IF DOES NOT EXIST
-                    printf("Data not found !\n");
-                    printf("Press Enter to continue ..."); getchar();
-                } else{ //IF EXISTS
-                    char cmd[20];
-                    printf("Input update type[add|remove|case insensitive]: "); scanf("%s", cmd);
-                    if (strcmp(cmd, "add") == 0){ //IF ADD
-                        getStock(stock);
-                    } else if (strcmp(cmd, "remove") == 0){ //IF REMOVE
-                        int removed;
-                        printf("Input stock to remove[1-50]: "); scanf("%d", &removed);
-                        if (removed < 1 || removed > 50){
-                            return 1;
-                        }
-                        removed -= root->stock;
-                        printf("Data updated successfully");
-                    } else{ //NEITHER OF THE COMMANDS/INCORRECT COMMANDS
-                        printf("What in the hell you doing bra\n");
+        case 3:{ //update stock
+            getGameName(root, title);
+            Node* searched = search(root, title);
+            if (searched == NULL){ //if does not exist
+                printf("Data not found !\n");
+                printf("Press Enter to continue ...\n"); getchar();
+            } else{ //if exists
+                char cmd[20];
+                int qty;
+                printf("Input update type[add|remove|case-insensitive]: "); scanf("%s", cmd);
+                if (strcmp(cmd, "add") == 0 || strcmp(cmd, "Add") == 0){ //add
+                    printf("Input stock to add[1-50]: "); scanf("%d", &qty);
+                    searched->stock += qty;
+                    printf("Data updated successfully !\n");
+                } else if (strcmp(cmd, "remove") == 0 || strcmp(cmd, "Remove") == 0){ //remove
+                    printf("Input stock to remove[1-50]: "); scanf("%d", &qty);
+                    if (qty > searched->stock){
+                        printf("Insufficient stock! Current stock: %d\n", searched->stock);
+                    } else if (qty < searched->stock){
+                        searched->stock -= qty;
+                        printf("Data updated successfully\n");
+                    } else{
+                        root = removeGame(root, title);
+                        printf("Data updated sucessfully\n");
+                        printf("%s is removed from the warehouse !\n", searched->title);
                     }
+                } else{ //incorrect cmd
+                    printf("bradar what is this\n");
                 }
             }
-            break;
-            case 4: //EXIT
-                break;
-            default: //ERROR MESSAGE
-                printf("Son\n");
-                break;
         }
-    } while (choice != 5);
+        break;
+        case 4: //exit
+            break;
+        default: //error message
+            printf("Try again\n");
+            break;
+        }
+    } while (choice != 4);
     freeGames(root);
 }
 
